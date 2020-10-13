9da75972a83723e948c1a8726d253e7f45eb2e80
Josep Egea <jse@josepegea.com>
2020-10-13 22:15:03 +0200

    Added mute audio tracks to all segments. Improved sponsors

--- 2018-02-RSpec/gen_cover.sh
+++ 2018-02-RSpec/gen_cover.sh
@@ -1,5 +1,7 @@
 ffmpeg -f lavfi -i color=size=1920x1080:color=black:d=5 \
+       -f lavfi -t 0.1 -i anullsrc \
        -filter_complex_script cover_script \
+       -map "[v]" -map "[a]" \
        -y output/build/cover.mp4
 
 
--- 2018-02-RSpec/gen_sponsors.sh
+++ 2018-02-RSpec/gen_sponsors.sh
@@ -1,8 +1,9 @@
-ffmpeg -r 1/5 \
-       -i ../shared/sponsors%01d.png \
-       -c:v libx264 \
-       -pix_fmt yuv420p \
-       -r 25 \
+ffmpeg -f lavfi -i color=size=1920x1080:color=black:d=20 \
+       -i ../shared/sponsors1.png \
+       -i ../shared/sponsors2.png \
+       -i ../shared/sponsors3.png \
+       -i ../shared/sponsors4.png \
+       -f lavfi -t 0.1 -i anullsrc \
+       -filter_complex_script sponsors_script \
+       -map "[v]" -map "[a]" \
        -y output/build/sponsors.mp4
-
-

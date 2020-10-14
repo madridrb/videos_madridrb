c74dfe08b8c15ec8858b410bae631e7e5cddd371
Josep Egea <jse@josepegea.com>
2020-10-14 22:19:26 +0200

    Organized in folders. Simplified Makefile

--- 2018-02-RSpec/camera/gen_camera.sh
+++ 2018-02-RSpec/camera/gen_camera.sh
@@ -2,4 +2,4 @@ ffmpeg -f concat \
        -safe 0 \
        -i camera_parts.txt \
        -c copy \
-       -y output/build/camera.MTS
+       -y ../output/build/camera.MTS
--- 2018-02-RSpec/full_video/gen_full_video.sh
+++ 2018-02-RSpec/full_video/gen_full_video.sh
@@ -2,4 +2,4 @@ ffmpeg -f concat \
        -safe 0 \
        -i full_video_parts.txt \
        -c copy \
-       -y output/full_video.mp4
+       -y ../output/full_video.mp4

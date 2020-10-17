474b7f3d208c0dda70a0fd925970b00721339481
Josep Egea <jse@josepegea.com>
2020-10-17 19:14:29 +0200

    Fixed fading

--- 2018-02-RSpec/video/gen_video.sh
+++ 2018-02-RSpec/video/gen_video.sh
@@ -2,4 +2,5 @@ ffmpeg -ss 00:05:42 -i ../output/build/camera.MTS\
        -ss 00:05:15 -i ../input/out1.ogv \
        -filter_complex_script video_script \
        -t 1:02:52 \
+       -map "[v]" -map "[a]" \
        -y ../output/build/video.mp4

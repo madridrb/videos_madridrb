ddc4c6cee73b13abbb49dfcedc83b043103a9e59
Josep Egea <jes@graymatter.local>
2021-12-09 20:14:47 +0100

    Move simple framing logic into a Ruby helper

--- lib/ffmpeg_utils.rb
+++ lib/ffmpeg_utils.rb
@@ -41,3 +41,19 @@ def banner(textfile, start_t, end_t, options = {})
     "if(between(t,#{start_t},#{start_t}+0.5),(t-#{start_t})/0.5,1)*" +
     "if(between(t,#{end_t}-0.5,#{end_t}),(#{end_t}-t)/0.5,1)'"
 end
+
+# Encloses 'video_stream' inside a slightly smaller frame inside a 1920x1080 stream
+# and puts a small logo and 2 texts below it.
+
+def simple_frame(video_stream, logo_stream, left_textfile, right_textfile, options = {})
+  prefix = options[:prefix] || 'simple_frame'
+  lines = []
+  lines << "color=black:s=1920x1080 [#{prefix}_bg];"
+  lines << "[#{logo_stream}]scale=w=-1:h=30 [#{prefix}_logo];"
+  lines << "[#{prefix}_bg][#{prefix}_logo] overlay=30:main_h-overlay_h-5 [#{prefix}_bglogo];"
+  lines << "[#{video_stream}]scale=w=-1:h=1040 [#{prefix}_stream];"
+  lines << "[#{prefix}_bglogo][#{prefix}_stream] overlay=main_w/2-overlay_w/2:0 [#{prefix}_composite];"
+  lines << "[#{prefix}_composite] drawtext=textfile=#{left_textfile}:fontcolor=0x888888:fontsize=14:x=80:y=h-25:fontfile=#{default_font} [#{prefix}_composite-text-1];"
+  lines << "[#{prefix}_composite-text-1] drawtext=textfile=#{right_textfile}:fontcolor=0x888888:fontsize=14:x=w-text_w-35:y=h-25:fontfile=#{default_font} [#{prefix}_final_frame];"
+  lines.join("\n")
+end
